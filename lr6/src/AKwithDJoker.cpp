9843e9fba152d94befb157c0f841f922a74a79f5
Vladimir Ivanov <mrcitron@mail.ru>
2018-05-20 12:41:41 +0000

    mod AKDj

--- lr6/src/AKwithDJoker.cpp
+++ lr6/src/AKwithDJoker.cpp
@@ -6,15 +6,12 @@ int main(){
     string text, temp;
     char razd;
     cin>>text>>temp>>razd;
-    if(checker(text)){
-        stringstream str_pat(temp);
-        vector<int> count(text.size(),0); //вектор, содержащий счётчики вхождений частей шаблона
-        vector<string> pattern; //вектор для частей шаблона
-        vector<int> lenght = spliter(str_pat, razd, pattern);//места, где части шаблона заканчиваются в самом шаблоне
-        findAllPosForDjoker(text, count, lenght);
-        printDj(count,text.size()-temp.size()+1,pattern.size());
-    }
-    else
-        cout<<"Вводятся латинские символы верхнего регистра!\n";
+    stringstream str_pat(temp);
+    vector<int> count(text.size(),0); //вектор, содержащий счётчики вхождений частей шаблона
+    vector<string> pattern; //вектор для частей шаблона
+    vector<int> lenght = spliter(str_pat, razd, pattern);//места, где части шаблона заканчиваются в самом шаблоне
+    findAllPosForDjoker(text, count, lenght);
+    printDj(count,text.size()-temp.size()+1,pattern.size());
+
     return 0;
 }
